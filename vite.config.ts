1ce9cb7306b40752280fdda71f3f7f4e98435abe
Christophe Renaud <christophe_renaud@outlook.com>
2025-11-18 15:45:22 -0500

    Corrected files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/portfolio-of-chris",
+	base: "/portfolio-of-chris/",
 	plugins: [reactRouter(), tailwindcss()],
 });
